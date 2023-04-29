ce274f930903979dea657d6a35c2e271a0033e80
fwl <18388504081@163.com>
2023-04-29 15:30:40 +0800

    增加后台页面订单列表接口

--- src/main/java/com/itheima/reggie/controller/OrderController.java
+++ src/main/java/com/itheima/reggie/controller/OrderController.java
@@ -1,11 +1,17 @@
 package com.itheima.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.R;
+import com.itheima.reggie.dto.DishDto;
+import com.itheima.reggie.entity.Dish;
 import com.itheima.reggie.entity.Orders;
 import com.itheima.reggie.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -18,16 +24,27 @@ public class OrderController {
 
     /**
      * 用户下单
+     *
      * @param orders
      * @return
      */
     @PostMapping("/submit")
-    public R<String> submit(@RequestBody Orders orders){
+    public R<String> submit(@RequestBody Orders orders) {
         log.info("订单数据: {}", orders);
         orderService.submit(orders);
         return R.success("下单成功");
     }
 
-    /*@GetMapping("/userPage")
-    public R<>*/
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String number, LocalDateTime beginTime, LocalDateTime endTime) {
+        // 构造分页构造器
+        Page<Orders> pageInfo = new Page<>(page, pageSize);
+        // 构造条件构造器
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
+        // 添加过滤条件
+        queryWrapper.eq(number != null, Orders::getNumber, number);
+        queryWrapper.between((beginTime != null && endTime != null), Orders::getOrderTime, beginTime, endTime);
+        orderService.page(pageInfo, queryWrapper);
+        return R.success(pageInfo);
+    }
 }
