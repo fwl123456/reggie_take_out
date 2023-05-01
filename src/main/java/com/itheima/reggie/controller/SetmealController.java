1fb9dd99f8a3b00270fa4ce525208344f314ca28
fwl <18388504081@163.com>
2023-05-02 00:12:01 +0800

    setmeal使用Spring Cache缓存套餐数据

--- src/main/java/com/itheima/reggie/controller/SetmealController.java
+++ src/main/java/com/itheima/reggie/controller/SetmealController.java
@@ -14,6 +14,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -42,8 +44,10 @@ public class SetmealController {
      *
      * @param setmealDto
      * @return
+     * CacheEvict: 表示当新增数据时删除setmealCache里面的所有缓存数据
      */
     @PostMapping
+    @CacheEvict(value = "setmealCache", allEntries = true)
     public R<String> save(@RequestBody SetmealDto setmealDto) {
         log.info("套餐信息: {}", setmealDto);
 
@@ -103,8 +107,10 @@ public class SetmealController {
      * 删除套餐
      * @param ids
      * @return
+     * CacheEvict: 表示当删除数据时删除setmealCache里面的所有缓存数据
      */
     @DeleteMapping
+    @CacheEvict(value = "setmealCache", allEntries = true)
     public R<String> delete(@RequestParam List<Long> ids){
         log.info("ids: {}", ids);
         setmealService.removeWithDish(ids);
@@ -136,7 +142,9 @@ public class SetmealController {
      * 根据条件来查询套餐数据
      * @param setmeal
      * @return
+     * @Cacheable 先从缓存里面查询对应数据是否存在 存在则返回数据 不存在则调用方法 并把方法返回值放入缓存
      */
+    @Cacheable(value = "setmealCache", key = "#setmeal.categoryId + '_' + #setmeal.status")
     @GetMapping("/list")
     public R<List<Setmeal>> list(Setmeal setmeal){
         LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
